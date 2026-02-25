bba65df4c92b14bb75c1001db9db9c7b36f3c50c
joydip-git <joydip.mondal@gmail.com>
2026-02-25 10:52:23 +0530

    styles applied on product list

--- codes/pms-app/src/app/modules/products/models/product.ts
+++ codes/pms-app/src/app/modules/products/models/product.ts
@@ -1,4 +1,15 @@
-export interface Product{
+// export interface Product{
+//     productId: number;
+//     productName: string;
+//     productCode: string;
+//     releaseDate: string;
+//     description: string;
+//     price: number;
+//     starRating: number;
+//     imageUrl: string;
+// }
+
+export type Product = {
     productId: number;
     productName: string;
     productCode: string;
