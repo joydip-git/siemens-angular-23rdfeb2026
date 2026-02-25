1f132300752304f53d3dd77626bc1301596abd1a
joydip-git <joydip.mondal@gmail.com>
2026-02-25 11:14:25 +0530

    directives

--- codes/pms-app/src/app/modules/products/components/product-list/product-list.ts
+++ codes/pms-app/src/app/modules/products/components/product-list/product-list.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { productRecords } from '../../../../../data/product-records';
+import { Product } from '../../models/product';
 
 @Component({
   selector: 'app-product-list',
@@ -8,5 +9,13 @@ import { productRecords } from '../../../../../data/product-records';
   styleUrl: './product-list.css',
 })
 export class ProductList {
-  products = signal(productRecords)
+  products = signal<Product[]>([])
+  constructor() {
+    setTimeout(
+      () => {
+        this.products.set(productRecords)
+      },
+      3000
+    )
+  }
 }
