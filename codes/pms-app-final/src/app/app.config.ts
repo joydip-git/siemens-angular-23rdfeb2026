42ab81be2ee19080bfe0af3c45b22e73939a99c7
joydip-git <joydip.mondal@gmail.com>
2026-02-27 12:35:40 +0530

    login component code created - part 1

--- codes/pms-app-final/src/app/app.config.ts
+++ codes/pms-app-final/src/app/app.config.ts
@@ -3,11 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideProductServiceProvider } from './config/constants';
+import { AuthService } from './modules/auth/services/auth-service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideProductServiceProvider(),
-    provideRouter(routes)
+    provideRouter(routes),
+    AuthService
   ]
 };
--- codes/pms-app-final/src/app/modules/auth/components/login/login.ts
+++ codes/pms-app-final/src/app/modules/auth/components/login/login.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Component, inject, OnDestroy } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { User } from '../../models/user';
+import { AuthService } from '../../services/auth-service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -7,13 +10,44 @@ import { FormControl, FormGroup } from '@angular/forms';
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
-export class Login {
-  loginForm = new FormGroup({
-    username: new FormControl(''),
-    password: new FormControl('')
-  })
+export class Login implements OnDestroy {
+  // loginForm = new FormGroup({
+  //   username: new FormControl(''),
+  //   password: new FormControl('')
+  // })
+
+  loginForm: FormGroup;
+  private builder = inject(FormBuilder)
+  private authSvc = inject(AuthService)
+  private sub?: Subscription;
+
+  constructor() {
+    this.loginForm = this.builder.group({
+      username: [''],
+      password: ['']
+    })
+  }
+  ngOnDestroy(): void {
+    this.sub?.unsubscribe()
+  }
 
   submit() {
-    console.log(this.loginForm.value);
+    if (window.confirm('Login?')) {
+      const user = <User>this.loginForm.value
+      this.sub = this.authSvc.loginUser(user).subscribe({
+        next: (resp) => {
+          const token = resp.data
+          if (token !== null) {
+            //save the token
+          } else {
+            window.alert(resp.message)
+          }
+        },
+        error: () => { },
+        complete: () => {
+          //redirect to the products by default if login was the originally requested url or to the originally requested url
+         }
+      })
+    }
   }
 }
